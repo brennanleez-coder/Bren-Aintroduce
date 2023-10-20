a9cab6632616dd8d4830f8b9702b96fa949ccfcf
brennanleez-coder <brennanlee95@gmail.com>
2023-10-20 21:28:13 +0800

    Increase height of input field

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <title>Bren-Aintroduce</title>
+      </head>
       <body>
         <Main />
         <NextScript />
